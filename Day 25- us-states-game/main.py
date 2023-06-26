0c31453d7d1b1cb4ca7974922bcdb55216ceb61e
Pichborith Kong <pichborith@gmail.com>
2023-06-26 14:27:00 -0700

    add day 26

--- Day 25- us-states-game/main.py
+++ Day 25- us-states-game/main.py
@@ -24,10 +24,11 @@ while len(guessed_states) < 50:
     ).title()
 
     if answer == "Exit":
-        missing_states = []
-        for state in states_list:
-            if state not in guessed_states:
-                missing_states.append(state)
+        # missing_states = []
+        # for state in states_list:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
+        missing_states = [state for state in states_list if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
