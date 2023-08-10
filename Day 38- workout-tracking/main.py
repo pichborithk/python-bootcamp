417d5dc342b24041a0d1dcfca993d195f87e5965
Pichborith Kong <pichborith@gmail.com>
2023-08-10 15:38:44 +0700

    finish day 38

--- Day 38- workout-tracking/main.py
+++ Day 38- workout-tracking/main.py
@@ -1,17 +1,28 @@
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
 APP_ID = os.getenv("NUTRITIONIX_APP_ID")
 APP_KEY = os.getenv("NUTRITIONIX_APP_KEY")
+TOKEN = os.getenv("SHEETY_TOKEN")
 
 nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheety_endpoint = (
+    "https://api.sheety.co/0f19e90cd91ebef484653657b6ae2102/workoutTracking/workouts"
+)
 
 exercises_text = input("Tell me which exercises you did: ")
 
-headers = {"x-app-id": APP_ID, "x-app-key": APP_KEY, "x-remote-user-id": "0"}
+nutritionix_headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": APP_KEY,
+    "x-remote-user-id": "0",
+}
+
+sheety_headers = {"Authorization": f"Bearer {TOKEN}"}
 
 exercises_data = {
     "query": exercises_text,
@@ -21,5 +32,26 @@ exercises_data = {
     "age": 34,
 }
 
-response = requests.post(url=nutritionix_endpoint, json=exercises_data, headers=headers)
-print(response.json())
+response = requests.post(
+    url=nutritionix_endpoint, json=exercises_data, headers=nutritionix_headers
+)
+result = response.json()
+
+today = datetime.now()
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today.strftime("%d/%m/%Y"),
+            "time": today.strftime("%X"),
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+
+    sheety_response = requests.post(
+        url=sheety_endpoint, json=sheet_inputs, headers=sheety_headers
+    )
+    # sheety_response = requests.post(url=sheety_endpoint, json=sheet_inputs, auth=(USERNAME, PASSWORD))
+    print(sheety_response.text)
