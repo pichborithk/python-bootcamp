f31507b1cace876e65c69438415d20b6abf1b8ef
Pichborith Kong <pichborith@gmail.com>
2023-09-17 17:45:14 -0700

    update demo

--- Python Demo/Control Flow/Control Flow.py
+++ Python Demo/Control Flow/Control Flow.py
@@ -11,9 +11,21 @@ elif a <= 1:
 else:
     print("else")
 
+match b:
+    case 0 | 1:
+        print("smaller")
+    case target if target in range(3, 10):
+        print("bigger")
+    case _:
+        print("equal")
 
 # Logical Operators
 if a > 1 and b > 1:
+    print("ok")
+else:
+    print("no")
+
+if 0 < a < 2:
     print("ok")
 else:
     print("no")
@@ -36,3 +48,25 @@ elif b in my_list:
     print("ok")
 else:
     print("else")
+
+
+# Ternary Operator
+
+c = 3 if a < b else 0
+
+# Ternary Operator using Tuples
+# In this example, we are using tuples to demonstrate ternary operator.
+# We are using tuple for selecting an item and if [a<b] is true it return 1, so element with 1 index will print
+# else if [a<b] is false it return 0, so element with 0 index will print.
+
+d, e = 10, 20
+
+print((e, d)[d < e])
+
+# Ternary Operator using Dictionary
+
+print({True: d, False: e}[d < e])
+
+# Ternary Operator using Lambda
+
+print((lambda: e, lambda: d)[d < e]())
