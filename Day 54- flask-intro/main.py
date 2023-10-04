578d8a0474f723ed4684b0e6cfdb6a7e6a463406
Pichborith Kong <pichborith@gmail.com>
2023-10-03 20:57:40 -0700

    update day 54

--- Day 54- flask-intro/main.py
+++ Day 54- flask-intro/main.py
@@ -6,12 +6,48 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(func):
+    def wrapper():
+        text = func()
+        return f"<b>{text}</b>"
+
+    return wrapper
+
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+
+    return wrapper
+
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+
+    return wrapper
+
+
 @app.route("/")
+@make_bold
+@make_underlined
+@make_emphasis
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return "Hello, World!"
+
+
+@app.route("/<name>/<int:post_id>")
+def greet(name, post_id):
+    return f"<h1>Hello, {name} this is post number {post_id}"
+
+
+@app.route("/path/<path:sub_path>")
+def show_sub_path(sub_path):
+    # show the sub path after /path/
+    return f"Sub path {sub_path}"
 
 
 # Can set environment variable FLASH_APP equal to file name
 # or use the code below to run flask app
 # if __name__ == "__main__":
-#     app.run()
+#     app.run(debug=True)
