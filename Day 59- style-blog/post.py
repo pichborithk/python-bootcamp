883cce77f4865a2c6d0f83ed3981c690759197e9
Pichborith Kong <pichborith@gmail.com>
2023-10-04 21:11:58 -0700

    add day 59

--- Day 59- style-blog/post.py
+++ Day 59- style-blog/post.py
@@ -2,11 +2,17 @@ import requests
 
 
 class Post:
-    def __init__(self, post_id, title, subtitle, body):
+    def __init__(
+        self, post_id, title, subtitle, body, date, author, image_alt, image_url
+    ):
         self.id = post_id
         self.title = title
         self.subtitle = subtitle
         self.body = body
+        self.date = date
+        self.author = author
+        self.image_alt = image_alt
+        self.image_url = image_url
 
 
 class PostManager:
@@ -18,8 +24,17 @@ class PostManager:
         return [post for post in self.posts if post.id == post_id][0]
 
     def get_all_posts(self):
-        response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
+        response = requests.get(url="https://api.npoint.io/eb6cd8a5d783f501ee7d")
         posts = response.json()
         for post in posts:
-            post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+            post_obj = Post(
+                post_id=post["id"],
+                title=post["title"],
+                subtitle=post["subtitle"],
+                body=post["body"],
+                date=post["date"],
+                image_alt=post["image_alt"],
+                author=post["author"],
+                image_url=post["image_url"]
+            )
             self.posts.append(post_obj)
--- Day 59- style-blog/server.py
+++ Day 59- style-blog/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 from post import PostManager
 
 app = Flask(__name__)
@@ -11,11 +12,20 @@ def home():
     return render_template("index.html", posts=post_manager.posts)
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 @app.route("/posts/<int:post_id>")
 def get_post(post_id):
     post = post_manager.get_post(post_id)
     return render_template("post.html", post=post)
-
 
 
 if __name__ == "__main__":
