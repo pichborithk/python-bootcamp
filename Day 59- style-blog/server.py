6e981e0c8f07af900a72d94992788e16749b357d
Pichborith Kong <pichborith@gmail.com>
2023-10-04 22:42:56 -0700

    add day 60

--- Day 59- style-blog/server.py
+++ Day 59- style-blog/server.py
@@ -1,4 +1,15 @@
-from flask import Flask, render_template
+import dotenv
+
+dotenv.load_dotenv()
+
+from flask import Flask, render_template, request
+import smtplib
+import os
+
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+RECEIVER_EMAIL = os.getenv("RECEIVER_EMAIL")
+PASSWORD = os.getenv("EMAIL_PASSWORD")
+
 
 from post import PostManager
 
@@ -17,9 +28,23 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=SENDER_EMAIL, password=PASSWORD)
+            connection.sendmail(
+                from_addr=SENDER_EMAIL,
+                to_addrs=RECEIVER_EMAIL,
+                msg=f"Subject:New Message\n\nName: {data['username']}\nEmail: {data['email']}\n"
+                    f"Phone: {data['phone']}\nMessage:{data['message']}",
+            )
+
+        return render_template("contact.html", msg_sent=True)
+
+    return render_template("contact.html", msg_sent=False)
 
 
 @app.route("/posts/<int:post_id>")
